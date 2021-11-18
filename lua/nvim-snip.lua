66986c6d5859ef52eaaa2bebb148b515925c0a24
PawelBis <bis1pawel1@gmail.com>
2021-11-18 11:46:36 +0100

    fix

--- lua/nvim-snip.lua
+++ lua/nvim-snip.lua
@@ -1,10 +1,7 @@
 local snips = {}
 
 function snips.snip()
-  local current_line = vim.api.nvim_get_current_line()
-  local cursor_line, cursor_clumn = vim.api.nvim_win_get_cursor(0)
-
-  vim.api.nvim_buf_set_lines(0, cursor_line, cursor_line, true, ["asdf"])
+  vim.api.nvim_buf_set_lines(0, cursor_line, cursor_line, v:true, ["asdf"])
 end
 
 return snips
