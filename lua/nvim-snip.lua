d4c6f1b716a12287b3475a069bdf1563d6af4412
PawelBis <bis1pawel1@gmail.com>
2021-11-18 08:36:07 +0100

    Fix nvim_win_get_cursor invocation

--- lua/nvim-snip.lua
+++ lua/nvim-snip.lua
@@ -2,7 +2,7 @@ local snips = {}
 
 function snips.snip()
   local current_line = vim.api.nvim_get_current_line()
-  local cursor_line, cursor_clumn = vim.api.nvim_win_get_cursor()
+  local cursor_line, cursor_clumn = vim.api.nvim_win_get_cursor(0)
 
   vim.api.nvim_buf_set_lines(0, cursor_line+1, cursor_line+1, 0, current_line)
 end
