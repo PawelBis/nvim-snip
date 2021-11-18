76aa127ef0776e185124572306f62bfc6f65582c
PawelBis <bis1pawel1@gmail.com>
2021-11-18 08:30:17 +0100

    Fix indexing

--- lua/nvim-snip.lua
+++ lua/nvim-snip.lua
@@ -1,6 +1,6 @@
-function snip()
+local function snip()
   local current_line = vim.api.nvim_get_current_line()
   local cursor_line, cursor_clumn = vim.api.nvim_win_get_cursor()
 
-  vim.api.nvim_buf_set_lines(0, cursor_line+1, cursor_line+1, false, current_line)
+  vim.api.nvim_buf_set_lines(0, cursor_line+1, cursor_line+1, 0, current_line)
 end
