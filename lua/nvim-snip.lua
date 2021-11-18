7979b5bde0171a4bf23f6f7e398586887789e26b
PawelBis <bis1pawel1@gmail.com>
2021-11-18 08:41:39 +0100

    ADd row

--- lua/nvim-snip.lua
+++ lua/nvim-snip.lua
@@ -4,7 +4,7 @@ function snips.snip()
   local current_line = vim.api.nvim_get_current_line()
   local cursor_line, cursor_clumn = vim.api.nvim_win_get_cursor(0)
 
-  vim.api.nvim_buf_set_lines(0, cursor_line, cursor_line, 0, {"asdf"})
+  vim.api.nvim_buf_set_lines(0, cursor_line.row, cursor_line.row, 0, {"asdf"})
 end
 
 return snips
